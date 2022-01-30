1f6d393ffcb68a54cf51f04ebf17b50ac842ddfb
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-01-30 18:22:50 +0900

    CRUD create, update, delete OK

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const { response } = require('express');
+const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
 const app = express()
 const port = 3000
 
 const mysql = require('mysql');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true}));//미들웨어로 설정
 
 const con = mysql.createConnection({
 	host: 'localhost',
@@ -12,22 +17,64 @@ const con = mysql.createConnection({
 	database: 'express_db'
 });
 
-con.connect(function(err){
-	if (err) throw err;
-	console.log('Connected');
-	const sql = "select * from users"
+// const sql = "INSERT INTO users(name,email) VALUES('tkim2','tkim@tkim.com')"
+
+// con.query(sql, function(err,result, fields){
+// 	if(err) throw err;
+// 	console.log(result);
+// })
+
+
+app.get('/', (req, res) => {
+	const sql = "select * from users";
 	con.query(sql, function (err, result, fields) {
-		if (err)
-			throw err;
-		console.log(result);
+		if (err) throw err;
+		res.render('index', {users : result});
 	});
 });
-app.get('/', (req, res) => {
-	const sql = "select * from users"
-	con.query(sql, function(err, result, fields) {
+
+app.post('/', (req, res) => {
+	const sql = "INSERT INTO users SET ?"
+	con.query(sql, req.body, function(err, result, fields){
 		if (err) throw err;
-		res.send(result);
-	})
-})
+		console.log(result);
+		res.redirect('/');
+	});
+});
+app.get('/create', (req, res) =>
+	res.sendFile(path.join(__dirname, 'index.html')))
+
+app.get('edit/:id', (req, res) => {
+	const sql ="SELECT * FROM users WHERE id = ?";
+	con.query(sql, [req.params.id], function (err, result, fields) {
+		if (err) throw err;
+		res.render('edit', {user : result});
+	});
+});
+
+app.post('/update/:id', (req, res) => {
+	const sql = "UPDATE users SET ? WHERE id = " + req.params.id;
+	con.query(sql, req.body, function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		res.redirect('/');
+	});
+});
+
+app.get('/delete/:id', (req, res) => {
+	const sql = "DELETE FROM users WHERE id = ?";
+	con.query(sql, [req.params.id], function(err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		res.redirect('/');
+	});
+});
+	// app.get('/', (req, res) => {
+		// 	const sql = "select * from users"
+		// 	con.query(sql, function(err, result, fields) {
+			// 		if (err) throw err;
+			// 		res.send(result);
+			// 	})
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port} !`))
